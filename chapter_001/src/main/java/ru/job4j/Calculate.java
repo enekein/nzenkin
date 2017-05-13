562099d16a8ba3dc52a99ec14b8f602d7b087c0f
enekein <nji1@mail.ru>
2017-05-13 16:44:07 +0400

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,26 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Nikita Zenkin (mailto:nji1@mail.ru)
+ * @version $Id$
+ * @since 0.1
+*/
+
 package ru.job4j;
 
+/**
+ * Class Calculate solution for part 001 lesson 1.
+ * @author nzenkin
+ * @since 13.05.2017
+*/
+
 class Calculate {
-    public static void main(String[] args){
+
+/**
+ * Output string to console.
+ * @param arg - arg.
+
+    public static void main(String[] arg){
         System.out.println("Hello world!");
     }
 }
