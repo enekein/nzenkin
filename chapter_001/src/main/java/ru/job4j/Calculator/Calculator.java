f99f8a14b6d95846e4706a388b1b6174a39d2971
enekein <nji1@mail.ru>
2017-05-18 07:39:09 +0400

    task 185

---
+++ chapter_001/src/main/java/ru/job4j/Calculator/Calculator.java
@@ -0,0 +1,66 @@
+package ru.job4j.calculator;
+
+/**
+ *@author Nikita Zenkin
+ *@since 14.05.2017
+ *@version 1
+*/
+
+public class Calculator {
+
+    /**
+     *Variable for result value.
+    */
+
+    private double result;
+
+    /**
+     * Method may you add 2 double digits.
+     *@param first - first double digit.
+     *@param second - second double digit.
+    */
+
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+    /**
+     * Method may you substruct 2 double digits.
+     *@param first - first double digit.
+     *@param second - second double digit.
+    */
+
+    public void substruct(double first, double second) {
+        this.result = first - second;
+    }
+
+    /**
+     * Method may you multyply 2 double digits.
+     *@param first - first double digit.
+     *@param second - second double digit.
+    */
+
+    public void multyply(double first, double second) {
+        this.result = first * second;
+    }
+
+    /**
+     * Method may you div 2 double digits.
+     *@param first - first double digit.
+     *@param second - second double digit.
+    */
+
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+
+    /**
+     * Method returns variable result from instance of Calculator class.
+     * @param none.
+     * @return - double.
+    */
+
+    public double getResult() {
+        return this.result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/Calculator/CalculatorTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *@author Nikita Zenkin
+ *@since 14.05.2017
+ *@version 1
+*/
+
+
+public class CalculatorTest {
+
+   /**
+     * Method tests method add.
+     * If add 1 + 1 the result must be 2.
+   */
+
+
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Method tests method multyply.
+     * If multyply 2 * 2 the result must be 4.
+    */
+
+    @Test
+    public void whenMultyplyTwoByTwoThenFour() {
+        Calculator calc = new Calculator();
+        calc.multyply(2D, 2D);
+        double result = calc.getResult();
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Method tests method substruct.
+     * If 2 - 1 the result must be 1.
+    */
+
+    @Test
+    public void whenSubstructTwoMinusOneThenOne() {
+        Calculator calc = new Calculator();
+        calc.substruct(2D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Method tests method div.
+     * If 4 / 2 the result must be 2.
+    */
+
+    @Test
+    public void whenDivFourByTwoThenTwo() {
+        Calculator calc = new Calculator();
+        calc.div(4D, 2D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+}
