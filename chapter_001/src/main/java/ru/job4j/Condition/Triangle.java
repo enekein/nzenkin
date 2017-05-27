7dce7ff35cdec3513becbbd31a94197e7c5ee779
enekein <nji1@mail.ru>
2017-05-27 10:51:24 +0400

    task 9461 v0.1

---
+++ chapter_001/src/main/java/ru/job4j/Condition/Triangle.java
@@ -0,0 +1,73 @@
+package ru.job4j.condition;
+
+ /**
+  *@author Nikita Zenkin.
+  *@since 19.05.2017.
+  *@version 1.
+ */
+
+public class Triangle {
+
+  /**
+   * @param a is coordinates of point 1 in triangle.
+  */
+
+  private Point a;
+
+  /**
+   * @param b is coordinates of point 2 in triangle.
+  */
+
+  private Point b;
+
+  /**
+   * @param c is coordinates of point 3 in triangle.
+  */
+
+  private Point c;
+
+  /**
+   *
+   * Constructor with 3 params(coordinates of points in triangle).
+   * @param a is first point coordinates of triangle.
+   * @param b is second point coordinates of triangle.
+   * @param c is third point coordinates of triangle.
+  */
+
+  public Triangle(Point a, Point b, Point c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  /**
+   * Method calculate side of triangle with knowing coordinates of points.
+   * @param a point which contains coordiantes x and y.
+   * @param b point which contains coordiantes x and y.
+   * @return double.
+  */
+
+  public double calculateSideLength(Point a, Point b) {
+	  return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
+  }
+
+  /**
+   * Method calculate area of triangle by Heron's formula with checking of triangle existing.
+   * @return double.
+  */
+
+  public double area() {
+	//Calculate sides.
+	double sideAB = calculateSideLength(a, b);
+	double sideBC = calculateSideLength(b, c);
+	double sideAC = calculateSideLength(a, c);
+	//check for triangle existing.
+	if ((sideAB + sideBC < sideAC) || (sideAC + sideAB < sideBC) || (sideBC + sideAC < sideAB)) {
+		return -1D;
+	}
+	//calculate half-perimeter
+    double p = (sideAB + sideAC + sideBC) / 2;
+	//using Heron formula and return double value
+	return Math.sqrt(p * (p - sideAB) * (p - sideAC) * (p - sideBC));
+  }
+}
---
+++ chapter_001/src/test/java/ru/job4j/Condition/TriangleTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.condition;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+
+/**
+  *@author Nikita Zenkin.
+  *@since 21.05.2017.
+  *@version 1.
+ */
+
+public class TriangleTest {
+
+	/**
+	 * Testing the situation when triangle is exists.
+	*/
+
+	@Test
+	public void whenTriangleIsExistsThenReturnArea() {
+		Point a = new Point(1, 1);
+		Point b = new Point(2, 1);
+		Point c = new Point(3, 4);
+		Triangle triangle = new Triangle(a, b, c);
+		double result = triangle.area();
+		double expected = 1.5D;
+		assertThat(result, closeTo(expected, 0.01));
+	}
+
+	/**
+	 * Testing the situation when triangle is not exists.
+	*/
+
+	@Test
+	public void whenOneSideGreaterThenSummOfTwoOtherSidesThenReturnMinusOne() {
+		Point a = new Point(0, 0);
+		Point b = new Point(0, 1);
+		Point c = new Point(0, 3);
+		Triangle triangle = new Triangle(a, b, c);
+		double result = triangle.area();
+		double expected = -1D;
+		assertThat(result, closeTo(expected, 0.01));
+	}
+}
+
