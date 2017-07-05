76129c982b3a3b58fb6f2ace1c9fa3e3c28a088b
enekein <nji1@mail.ru>
2017-07-05 18:50:30 +0400

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_005Lite/src/main/java/ru/job4j/sort/Sort.java
+++ chapter_005Lite/src/main/java/ru/job4j/sort/Sort.java
@@ -2,14 +2,18 @@ package ru.job4j.sort;
 
 import java.util.List;
 import java.util.Set;
+import java.util.Comparator;
 import java.util.TreeSet;
 
 /**
  * @author Nikita Zenkin.
- * @version 1.
+ * @version 2.
  * @since 26.06.2017.
  */
 class Sort {
+    /**
+     * Static class for user.
+     */
     public static class User implements Comparable<User> {
         /**
          * Age for user. int
@@ -21,7 +25,7 @@ class Sort {
         private String name;
 
         /**
-         * Constructor
+         * Constructor.
          * @param age int.
          * @param name String.
          */
@@ -31,26 +35,109 @@ class Sort {
         }
 
         /**
+         * Get name.
+         * @return String.
+         */
+        String getName() {
+            return this.name;
+        }
+
+        /**
+         * Get age.
+         * @return int.
+         */
+        int getAge() {
+            return this.age;
+        }
+
+        /**
          * Override method compareTo for User.
          * @param user User.
          * @return int.
          */
         @Override
         public int compareTo(User user) {
-            if(this.age > user.age)
+            if (this.age > user.age) {
                 return 1;
-            else if(this.age < user.age)
+            } else if (this.age < user.age) {
                 return -1;
-            else
+            } else {
                 return 0;
+            }
         }
-}
-static class SortUser {
-    Set<User> sort (List<User> arrayList) {
-        Set<User> set = new TreeSet<>();
-        set.addAll(arrayList);
-        return set;
     }
-}
 
+    /**
+     * Static class for sorting users.
+     */
+    static class SortUser {
+        /**
+         * TreeSet.
+         * @param arrayList List User.
+         * @return Set User.
+         */
+        Set<User> sort(List<User> arrayList) {
+            Set<User> set = new TreeSet<>();
+            set.addAll(arrayList);
+            return set;
+        }
+
+        /**
+         * Sort by name.
+         * @param arrayList List User.
+         * @return List User.
+         */
+        List<User> sortNameLength(List<User> arrayList) {
+            arrayList.sort(
+                    new Comparator<User>() {
+                        @Override
+                        public int compare(User o1, User o2) {
+                            if (o1.name.length() > o2.name.length()) {
+                                return 1;
+                            } else if (o1.name.length() < o2.name.length()) {
+                                return -1;
+                            } else {
+                                return 0;
+                            }
+                        }
+                    }
+            );
+            return arrayList;
+        }
+
+        /**
+         * Sort by all fields.
+         * @param arrayList List User.
+         * @return List User.
+         */
+        List<User> sortByAllFields(List<User> arrayList) {
+            arrayList.sort(new SortByAllFields());
+            return arrayList;
+        }
+    }
+
+    /**
+     * Static class with comparator.
+     */
+    static class SortByAllFields implements Comparator<User> {
+        /**
+         * Method compare for sorting in list.
+         * @param o1 User.
+         * @param o2 User.
+         * @return int.
+         */
+        @Override
+        public int compare(User o1, User o2) {
+            int temp = o1.name.compareTo(o2.name);
+            if (temp == 0) {
+                if (o1.age > o2.age) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else {
+              return temp;
+            }
+        }
+    }
 }
--- chapter_005Lite/src/test/java/ru/job4j/sort/SortTest.java
+++ chapter_005Lite/src/test/java/ru/job4j/sort/SortTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.sort;
 
-
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +10,7 @@ import static org.junit.Assert.assertThat;
 
 /**
  * @author Nikita Zenkin.
- * @version 1.
+ * @version 2.
  * @since 26.06.2017.
  */
 public class SortTest {
@@ -31,5 +30,45 @@ public class SortTest {
         expected.add(new Sort.User(20, "Temmy"));
         expected.add(new Sort.User(26, "Demmy"));
         assertThat(result, is(expected));
+    }
+
+    /**
+     * Test method sortNameLength in SortUser.
+     */
+    @Test
+    public void whenSortArrayListWithNameLengthThenReturnArrayListSortedByNameLength() {
+        Sort.SortUser sortUser = new Sort.SortUser();
+        List<Sort.User> arrayList = new ArrayList<>();
+        arrayList.add(new Sort.User(20, "Temmyd"));
+        arrayList.add(new Sort.User(26, "Demm"));
+        arrayList.add(new Sort.User(18, "Cemmy"));
+        List<Sort.User> result = sortUser.sortNameLength(arrayList);
+        List<Sort.User> expected = new ArrayList<>();
+        expected.add(new Sort.User(26, "Demm"));
+        expected.add(new Sort.User(18, "Cemmy"));
+        expected.add(new Sort.User(20, "Temmyd"));
+        for (int index = 0; index < result.size(); index++) {
+            assertThat(result.get(index).getName(), is(expected.get(index).getName()));
+        }
+    }
+
+    /**
+     * Test method sortByAllFields int SortUser.
+     */
+    @Test
+    public void whenSortArrayListWithNameAndAgeThenReturnArrayListSortedByNameAndAge() {
+        Sort.SortUser sortUser = new Sort.SortUser();
+        List<Sort.User> arrayList = new ArrayList<>();
+        arrayList.add(new Sort.User(20, "Temmyd"));
+        arrayList.add(new Sort.User(26, "Demmy"));
+        arrayList.add(new Sort.User(18, "Demmy"));
+        List<Sort.User> result = sortUser.sortByAllFields(arrayList);
+        List<Sort.User> expected = new ArrayList<>();
+        expected.add(new Sort.User(18, "Demmy"));
+        expected.add(new Sort.User(26, "Demmy"));
+        expected.add(new Sort.User(20, "Temmyd"));
+        for (int index = 0; index < result.size(); index++) {
+            assertThat(result.get(index).getName(), is(expected.get(index).getName()));
+        }
     }
 }
