0e9aa11ab7e3f705b9a60e8e502a44913d79f53f
Nikita <nji1@mail.ru>
2017-10-23 11:56:04 +0400

    Update For codind at work :).java

--- chapter_005/For codind at work :).java
+++ chapter_005/For codind at work :).java
@@ -14,4 +14,40 @@ throw new IllegalStateException("Queue is full! You need to remove something fir
 }       
 return false;   
 }      
-E element() {        return super.getElements[super.getLastIndex() - 1].getContainer();    }        boolean offer(E e) {        try {            super.fillNextValue();            super.getElements[super.getLastIndex()].setContainer(e);            super.incrementIndex();            return true;        } catch (ArrayIndexOutOfBoundsException aioobe) {            return false;        }    }        E peek() {        if (!super.getElements.equals(null)) {            return super.getElements[super.getLastIndex() - 1].getContainer();        } else {            return null;        }    }        E poll() {        if (!super.getElements.equals(null)) {            E temp = (E) super.getElements[super.getLastIndex() - 1].getContainer();            super.getElements[super.getLastIndex() - 2].setNext = null;            super.decrementIndex();            return temp;        } else {            return null;        }    }        E remove() {        E temp = (E) super.getElements[super.getLastIndex() - 1].getContainer();        super.getElements[super.getLastIndex() - 2].setNext = null;        super.decrementIndex();        return temp;    }}
+E element() {        
+	return super.getElements[super.getLastIndex() - 1].getContainer();    
+}        
+	boolean offer(E e) {        
+		try {            
+			super.fillNextValue();            
+			super.getElements[super.getLastIndex()].setContainer(e);            
+			super.incrementIndex();            
+			return true;        
+		} catch (ArrayIndexOutOfBoundsException aioobe) {            
+			return false;        
+		}    
+	}        
+	E peek() {        
+		if (!super.getElements.equals(null)) {            
+			return super.getElements[super.getLastIndex() - 1].getContainer();        
+		} else {            
+			return null;       
+		}    
+	}        
+	E poll() {        
+		if (!super.getElements.equals(null)) {            
+			E temp = (E) super.getElements[super.getLastIndex() - 1].getContainer();          
+			super.getElements[super.getLastIndex() - 2].setNext = null;            
+			super.decrementIndex();            
+			return temp;        
+		} else {            
+			return null;       
+		}   
+	}        
+	E remove() {        
+		E temp = (E) super.getElements[super.getLastIndex() - 1].getContainer();       
+		super.getElements[super.getLastIndex() - 2].setNext = null;       
+		super.decrementIndex();       
+		return temp;    
+	}
+}
